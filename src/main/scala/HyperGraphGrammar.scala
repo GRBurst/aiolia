c3fb0064673b26820e8fbeda32a0a3cc1a994e59
johannes karoff <johannes@karoff.net>
2016-03-21 18:43:54 +0100

    format sources

--- src/main/scala/HyperGraphGrammar.scala
+++ src/main/scala/HyperGraphGrammar.scala
@@ -7,7 +7,7 @@ object Types {
 import Types._
 
 case class HyperGraph(hyperEdges: Set[HyperEdge] = Set.empty, edges: Set[Edge] = Set.empty) {
-  def vertices:Set[Vertex] = {
+  def vertices: Set[Vertex] = {
     val hyperVertices = hyperEdges.flatMap { case HyperEdge(_, in, out) => in ++ out }
     val edgeVertices = edges.flatMap { case Edge(in, out) => Set(in, out) }
     hyperVertices ++ edgeVertices
@@ -19,26 +19,26 @@ case class HyperGraph(hyperEdges: Set[HyperEdge] = Set.empty, edges: Set[Edge] =
   }
 }
 
-case class MultiPointedHyperGraph(in: List[Vertex], out:List[Vertex], hyperGraph: HyperGraph) {
+case class MultiPointedHyperGraph(in: List[Vertex], out: List[Vertex], hyperGraph: HyperGraph) {
   def vertices = hyperGraph.vertices
   def edges = hyperGraph.edges
   def hyperEdges = hyperGraph.hyperEdges
 }
 
 object MultiPointedHyperGraph {
-  def apply(in: List[Vertex] = Nil, out:List[Vertex] = Nil, hyperEdges: Set[HyperEdge] = Set.empty, edges: Set[Edge] = Set.empty):MultiPointedHyperGraph = {
+  def apply(in: List[Vertex] = Nil, out: List[Vertex] = Nil, hyperEdges: Set[HyperEdge] = Set.empty, edges: Set[Edge] = Set.empty): MultiPointedHyperGraph = {
     MultiPointedHyperGraph(in, out, HyperGraph(hyperEdges, edges))
   }
 }
 
-case class HyperEdge(label: Label, in:List[Vertex], out:List[Vertex])
+case class HyperEdge(label: Label, in: List[Vertex], out: List[Vertex])
 
 case class Graph(edges: Set[Edge]) {
-  def vertices:Set[Vertex] = edges.flatMap { case Edge(in, out) => Set(in, out) }
-  assert( (0 until vertices.size).forall(vertices contains Vertex(_)), s"${vertices}" )
+  def vertices: Set[Vertex] = edges.flatMap { case Edge(in, out) => Set(in, out) }
+  assert((0 until vertices.size).forall(vertices contains Vertex(_)), s"${vertices}")
 }
 
-case class Edge(in:Vertex, out:Vertex) {
+case class Edge(in: Vertex, out: Vertex) {
   override def toString = s"$in -> $out"
 }
 
@@ -47,21 +47,22 @@ case class Grammar(axiom: HyperGraph, productions: Map[Label, MultiPointedHyperG
   def expand = {
     var current = axiom
     val autoId = new AutoId(axiom.vertices.size)
-    while( current.hyperEdges.nonEmpty ) {
+    while (current.hyperEdges.nonEmpty) {
       val lhs = current.hyperEdges.head
       val rhs = productions(lhs.label)
 
       val newVertices = (rhs.vertices -- (rhs.in ++ rhs.out)).map(_.label -> Vertex(autoId.nextId)).toMap
       val existVertices = rhs.in.map(_.label).zip(lhs.in).toMap ++ rhs.out.map(_.label).zip(lhs.out).toMap
-      val vertices:Map[Label, Vertex] = newVertices ++ existVertices
+      val vertices: Map[Label, Vertex] = newVertices ++ existVertices
 
-      val edges = rhs.edges.map{ case Edge(Vertex(in), Vertex(out)) => Edge(vertices(in), vertices(out)) }
+      val edges = rhs.edges.map { case Edge(Vertex(in), Vertex(out)) => Edge(vertices(in), vertices(out)) }
 
-      val hyperEdges = rhs.hyperEdges.map{ case HyperEdge(label, in, out) => HyperEdge(label, in.map(v => vertices(v.label)), out.map(v => vertices(v.label))) }
+      val hyperEdges = rhs.hyperEdges.map { case HyperEdge(label, in, out) => HyperEdge(label, in.map(v => vertices(v.label)), out.map(v => vertices(v.label))) }
 
       current = HyperGraph(
         current.hyperEdges ++ hyperEdges - lhs,
-        current.edges ++ edges)
+        current.edges ++ edges
+      )
     }
 
     current.toGraph
@@ -78,4 +79,3 @@ class AutoId(var start: Int = 0) {
     current
   }
 }
-
--- src/test/scala/HyperGraphGrammarSpec.scala
+++ src/test/scala/HyperGraphGrammarSpec.scala
@@ -3,8 +3,8 @@ package aiolia.hypergraphgrammar
 import Types._
 
 object Helpers {
-  implicit def VertexTupleToEdge(tuple:(Int, Int)) = Edge(Vertex(tuple._1), Vertex(tuple._2))
-  implicit def IntToVertex(i:Int) = Vertex(i)
+  implicit def VertexTupleToEdge(tuple: (Int, Int)) = Edge(Vertex(tuple._1), Vertex(tuple._2))
+  implicit def IntToVertex(i: Int) = Vertex(i)
 }
 
 import Helpers._
@@ -17,7 +17,7 @@ class HyperGraphGrammarSpec extends org.specs2.mutable.Specification {
       val axiom = HyperGraph(Set(h1, h2))
 
       val g = Grammar(axiom, Map(
-        1 -> MultiPointedHyperGraph(in = List(1), out = List(2,3), edges = Set(1 -> 3, 3 -> 4, 4 -> 5, 3 -> 5, 5 -> 2))
+        1 -> MultiPointedHyperGraph(in = List(1), out = List(2, 3), edges = Set(1 -> 3, 3 -> 4, 4 -> 5, 3 -> 5, 5 -> 2))
       ))
 
       val e1 = g.expand
@@ -32,7 +32,7 @@ class HyperGraphGrammarSpec extends org.specs2.mutable.Specification {
       val axiom = HyperGraph(Set(h1, h2))
 
       val g = Grammar(axiom, Map(
-        1 -> MultiPointedHyperGraph(in = List(10), out = List(20,30), edges = Set(10 -> 30, 30 -> 40, 40 -> 50, 30 -> 50, 50 -> 20))
+        1 -> MultiPointedHyperGraph(in = List(10), out = List(20, 30), edges = Set(10 -> 30, 30 -> 40, 40 -> 50, 30 -> 50, 50 -> 20))
       ))
 
       g.expand mustEqual Graph(Set(6 -> 2, 2 -> 4, 2 -> 3, 1 -> 5, 1 -> 6, 0 -> 1, 5 -> 6, 4 -> 1, 0 -> 2, 3 -> 4))
@@ -44,7 +44,7 @@ class HyperGraphGrammarSpec extends org.specs2.mutable.Specification {
 
       val g = Grammar(axiom, Map(
         1 -> MultiPointedHyperGraph(in = List(0, 1), out = List(2, 3), edges = Set(0 -> 1, 2 -> 3), hyperEdges = Set(HyperEdge(2, List(0, 1), List(2)))),
-          2 -> MultiPointedHyperGraph(in = List(0, 1), out = List(2), edges = Set( 1 -> 2 ))
+        2 -> MultiPointedHyperGraph(in = List(0, 1), out = List(2), edges = Set(1 -> 2))
       ))
 
       g.expand mustEqual Graph(Set(0 -> 1, 1 -> 2, 2 -> 3))
