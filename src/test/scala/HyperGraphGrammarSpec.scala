4557fddd93778837c760021fd5079ad60b4ed875
Felix Dietze <felix@beerleader.net>
2016-03-21 18:15:30 +0100

    expand from empty hyperedge axiom

--- src/test/scala/HyperGraphGrammarSpec.scala
+++ src/test/scala/HyperGraphGrammarSpec.scala
@@ -35,8 +35,7 @@ class HyperGraphGrammarSpec extends org.specs2.mutable.Specification {
         1 -> MultiPointedHyperGraph(in = List(10), out = List(20,30), edges = Set(10 -> 30, 30 -> 40, 40 -> 50, 30 -> 50, 50 -> 20))
       ))
 
-      val e1 = g.expand
-      e1 mustEqual Graph(Set(6 -> 2, 2 -> 4, 2 -> 3, 1 -> 5, 1 -> 6, 0 -> 1, 5 -> 6, 4 -> 1, 0 -> 2, 3 -> 4))
+      g.expand mustEqual Graph(Set(6 -> 2, 2 -> 4, 2 -> 3, 1 -> 5, 1 -> 6, 0 -> 1, 5 -> 6, 4 -> 1, 0 -> 2, 3 -> 4))
     }
 
     "expand hyperedges" >> {
@@ -48,8 +47,17 @@ class HyperGraphGrammarSpec extends org.specs2.mutable.Specification {
           2 -> MultiPointedHyperGraph(in = List(0, 1), out = List(2), edges = Set( 1 -> 2 ))
       ))
 
-      val e1 = g.expand
-      e1 mustEqual Graph(Set(0 -> 1, 1 -> 2, 2 -> 3))
+      g.expand mustEqual Graph(Set(0 -> 1, 1 -> 2, 2 -> 3))
+    }
+
+    "empty hyperedge axiom" >> {
+      val axiom = HyperGraph(Set(HyperEdge(1, Nil, Nil)))
+
+      val g = Grammar(axiom, Map(
+        1 -> MultiPointedHyperGraph(in = List(), out = List(), edges = Set(0 -> 1, 1 -> 2, 2 -> 1))
+      ))
+
+      g.expand mustEqual Graph(Set(0 -> 1, 1 -> 2, 2 -> 1))
     }
   }
 }
