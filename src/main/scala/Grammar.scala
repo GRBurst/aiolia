d032445aee2597938942253d60084b9a18ebf0cf
Felix Dietze <felix@beerleader.net>
2016-04-26 13:25:33 +0200

    axiom must not have connectors

--- src/main/scala/Grammar.scala
+++ src/main/scala/Grammar.scala
@@ -11,7 +11,7 @@ case class Grammar[+V, +E](axiom: Graph[V, E], productions: Map[Label, Graph[V, 
     rhs.isDefined && (nonTerminal.connectors.size == rhs.get.connectors.size)
   }, "All nonterminals on the rhs need to have an equivalent on the lhs")
   assert(!dependencyGraph.hasCycle, "this grammer contains cycles, which it shouldn't, so shit see this instead.")
-  //TODO: assert axiom must not have connectors
+  assert(axiom.connectors.isEmpty, "Axiom must not have connectors")
 
   def addProduction[V1 >: V, E1 >: E](production: (Label, Graph[V1, E1])): Grammar[V1, E1] = {
     assert(!(productions.keys.toSet contains production._1), s"productions already contain a rule with label ${production._1}\nproductions:\n${productions.mkString("\n")}")
