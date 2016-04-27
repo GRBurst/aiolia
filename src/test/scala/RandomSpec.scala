f396778dcb4ddde43e6f36409a2305547d4e5322
johannes karoff <johannes@karoff.net>
2016-04-27 15:06:20 +0200

    test for random select multiple elements

--- src/test/scala/RandomSpec.scala
+++ src/test/scala/RandomSpec.scala
@@ -37,6 +37,19 @@ class RandomSpec extends org.specs2.mutable.Specification with org.specs2.mock.M
         rand.r.nextInt(5) returns 2
         rand.select(List(1, 2, 3, 4, 5)) mustEqual 3
       }
+
+      "multiple elements" >> {
+        val rand = new Random(seed = 0)
+        val l = List(1, 2, 3, 4, 5)
+        val r = rand.select(l, 3)
+        r.size mustEqual 3
+
+        //TODO: make it work with mock?
+        // val rand = randMock
+        // val l = List(1, 2, 3, 4, 5)
+        // rand.r.shuffle(l)(collection.immutable.List.canBuildFrom[Int]) returns l
+        // rand.select(l, 3) mustEqual List(1, 2, 3)
+      }
     }
   }
 }
