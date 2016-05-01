bcdf551aca63872487c43fc2a7baa3ab962ee3ef
GRBurst <JuliusE89@gmail.com>
2016-05-02 00:30:20 +0200

    Advanced bug discovery on sbt run

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,10 +10,12 @@ import aiolia.helpers._
 object Main extends App {
   println("Good Bye Aiolia")
 
-  val grammar = Mutation.mutateDirectedConnected(Grammar.minimal, Random(1), 100)
+  val grammar = Mutation.mutateDirectedConnected(Grammar.minimal, Random(3), 100)
   println(grammar.uniqueVertices)
   write("grammar.dot", DOTExport.toDOT(grammar))
   write("graph.dot", DOTExport.toDOT(grammar.expand))
+
+  for(i <- 0 to 20) try {Mutation.mutateDirectedConnected(Grammar.minimal,Random(i),1000)} catch { case _:IsotopicException$ => ; case e:AssertionError => {println("\n\nASSERTION\n"+e.getMessage);Thread.sleep(2000)}}
 
   def write(filename: String, content: String) {
     Some(new java.io.PrintWriter(filename)).foreach{ p =>
