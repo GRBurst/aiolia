17df0495f414fae0ca7bd6e2fe0decc256090212
Felix Dietze <felix@beerleader.net>
2016-05-24 13:25:05 +0200

    fix distance

--- src/main/scala/util/Vec2.scala
+++ src/main/scala/util/Vec2.scala
@@ -1,10 +1,10 @@
 package aiolia.util
 
 case class Vec2(x: Int, y: Int) {
-  def +(that:Vec2) = Vec2(x + that.x, y + that.y)
-  def -(that:Vec2) = Vec2(x - that.x, y - that.y)
+  def +(that: Vec2) = Vec2(x + that.x, y + that.y)
+  def -(that: Vec2) = Vec2(x - that.x, y - that.y)
 
-  def distance(that:Vec2) = Math.sqrt(Math.pow(this.x - that.y, 2) + Math.pow(this.y - that.y, 2))
+  def distance(that: Vec2) = Math.sqrt((this.x - that.x) * (this.x - that.x) + (this.y - that.y) * (this.y - that.y))
   def angle = Math.atan2(y, x);
 
   override def toString = s"($x,$y)"
