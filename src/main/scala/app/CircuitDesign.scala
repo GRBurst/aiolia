bf9c7b314c29ad002be4113e5b45a389b5335cbe
GRBurst <JuliusE89@gmail.com>
2016-07-14 12:58:40 +0200

    adaptive md5 examples

--- src/main/scala/app/CircuitDesign.scala
+++ src/main/scala/app/CircuitDesign.scala
@@ -11,9 +11,11 @@ import scala.concurrent.duration._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
+import java.security.MessageDigest
+
 object CircuitDesign extends App {
   val ga = GeneticAlgorithm(CircuitDesignConfig())
-  ga.runFor(100)
+  ga.runFor(Duration.Inf)
 }
 
 case class CircuitDesignConfig() extends Config[Graph[Nothing, Nothing]] with CircuitConfig {
@@ -21,26 +23,64 @@ case class CircuitDesignConfig() extends Config[Graph[Nothing, Nothing]] with Ci
   type Genotype = Graph[Nothing, Nothing]
   type Phenotype = Image
 
-  override val populationSize: Int = 50
+  override val populationSize: Int = 100
   override val tournamentSize = 3
-  override def mutationCount(g: Genotype) = 2
+  override def mutationCount(g: Genotype) = random.nextInt(1, 4)
+  // override def mutationCount(g: Genotype) = 1
 
   val seed = 0
-  override val parallel: Boolean = false
+  override val parallel: Boolean = true
 
-  val inputs = VL(0, 1)
-  val outputs = VL(2)
+  val inputs = List.tabulate(128)(x => v(x))
+  val outputs = List.tabulate(128)(x => v(x + 128))
   val baseGenotype = Graph(vertices = (inputs ++ outputs).toSet)
 
-  val examples = (
-    Array(false, false) -> Array(false) ::
-    Array(false, true) -> Array(true) ::
-    Array(true, false) -> Array(true) ::
-    Array(true, true) -> Array(false) ::
-    // Array(true) -> Array(false) ::
-    // Array(false) -> Array(true) ::
-    Nil
-  )
+  def byteToBoolArray(inBytes: Array[Byte]): Array[Boolean] = {
+    val outBools = new Array[Boolean](inBytes.size * 8)
+    for ((byte, index) <- inBytes zipWithIndex) {
+      for (bit <- 0 until 8) {
+        outBools(index * 8 + (7 - bit)) = ((byte >> bit) & 1) == 1
+      }
+    }
+    outBools
+  }
+
+  def calcMd5Sum(inBytes: Array[Byte]): Array[Byte] = {
+    val md = MessageDigest.getInstance("MD5")
+    md.update(inBytes)
+    md.digest()
+  }
+
+  def genExamples(n: Int) = {
+    val random = new scala.util.Random(seed)
+    List.fill(n){
+      val in = new Array[Byte](16)
+      random.nextBytes(in)
+      val out = calcMd5Sum(in)
+      byteToBoolArray(in) -> byteToBoolArray(out)
+    }
+  }
+
+  override def afterFitness(population: Population, fitness: (Genotype) => Double, generation: Int) {
+    val best = population.map(fitness).max
+    if (best >= examples.size * outputs.size) {
+      exampleCount += 1
+      examples = genExamples(exampleCount)
+    }
+  }
+
+  var exampleCount = 1
+  var examples = genExamples(exampleCount)
+  // val examples = (
+  //   // Array(
+  //   Array(false, false) -> Array(false) ::
+  //   Array(false, true) -> Array(true) ::
+  //   Array(true, false) -> Array(true) ::
+  //   Array(true, true) -> Array(false) ::
+  //   // Array(true) -> Array(false) ::
+  //   // Array(false) -> Array(true) ::
+  //   Nil
+  // )
 
   def calculateFitness(g: Genotype, prefix: String): Double = {
     val circuit = Circuit(inputs, outputs, g)
@@ -52,8 +92,8 @@ case class CircuitDesignConfig() extends Config[Graph[Nothing, Nothing]] with Ci
       }
     }
     val circuitSizeScore = 1.0 / (g.vertices.size)
-    score + circuitSizeScore
+    (score + circuitSizeScore)
   }
 
-  override def stats(best: Genotype) = s", gates: ${best.vertices.size - inputs.size}, e: ${best.edges.size}, g: ${best}"
+  override def stats(best: Genotype) = s"/ ${(examples.size * outputs.size)}[$exampleCount], gates: ${best.vertices.size - inputs.size}, e: ${best.edges.size}"
 }
